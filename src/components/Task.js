eb2b0e88ff95dcd00b5d877a97186acdac84a870
tomasjimenez <tomas.jimenez.flores@gmail.com>
2020-06-27 19:05:32 -0500

    modificación del front-end

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,16 @@
 import React from "react";
+import axios from "axios";
 
 const Task = (props) => {
+
+  const ENDPOINT = "https://to-do-devf-tomas.firebaseio.com/task/";
+
+  const deleteTask = (id) => {
+    axios.delete(`${ENDPOINT}${id}.json`)
+    .then(() => props.getTasks()) 
+    .catch((error) => alert('Ocurrió un error al eliminar la tarea' + error)) 
+  }
+
   return (
     <div className="card">
       <h1>{props.title}</h1>
@@ -9,7 +19,7 @@ const Task = (props) => {
       <p>{props.done}</p>
       <div className="row">
         <div className="col-3">
-          <button type="button" className="btn btn-danger">
+          <button type="button" className="btn btn-danger" onClick={() => deleteTask(props.id) }>
             Eliminar
           </button>
         </div>
